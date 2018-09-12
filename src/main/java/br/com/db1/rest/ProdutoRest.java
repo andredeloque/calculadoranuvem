2570fee4e9049b9312543c4aca2bc021af664834
andredeloque <andredeloque@gmail.com>
2018-09-12 16:00:30 -0300

    correcao produto

--- src/main/java/br/com/db1/rest/ProdutoRest.java
+++ src/main/java/br/com/db1/rest/ProdutoRest.java
@@ -1,8 +1,11 @@
 package br.com.db1.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import br.com.db1.model.Produto;
 
@@ -15,10 +18,10 @@ public class ProdutoRest {
 	public Produto getProdutoJSON() {
 
 		Produto produto = new Produto();
-		produto.setNome("Moto X - Android");
+		produto.setNome("motox");
 		produto.setQuantidade(10);
 
-		return produto;
+return produto;
 
 	}
 	@POST
